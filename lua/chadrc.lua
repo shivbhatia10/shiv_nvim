08b94e668f5dd69813a39ce017badc89527c057d
Shiv Bhatia <sbhatia@palantir.com>
2026-06-11 11:44:42 +0100

    Full path in status bar

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -15,10 +15,25 @@ M.base46 = {
 }
 
 -- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
--- }
+
+-- Show the file's home-relative path in the statusline instead of just its name
+M.ui = {
+  statusline = {
+    modules = {
+      file = function()
+        local stl = require "nvchad.stl.utils"
+        local icon = stl.file()[1] -- reuse NvChad's devicon logic
+        local path = vim.api.nvim_buf_get_name(stl.stbufnr())
+        local name = (path == "" and "Empty") or vim.fn.fnamemodify(path, ":~")
+
+        local sep_style = require("nvconfig").ui.statusline.separator_style
+        local separators = (type(sep_style) == "table" and sep_style) or stl.separators[sep_style]
+        local pad = sep_style == "default" and " " or ""
+
+        return "%#St_file# " .. icon .. " " .. name .. pad .. "%#St_file_sep#" .. separators.right
+      end,
+    },
+  },
+}
 
 return M
