5039512a2f4868f225523447e74d2a5d84c5f1ce
Shiv Bhatia <sbhatia@palantir.com>
2026-06-11 11:50:54 +0100

    telescope ff finds hidden files except in .git/

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,4 +6,9 @@ local map = vim.keymap.set
 
 map("i", "jk", "<ESC>")
 
+-- find files including hidden/dotfiles (e.g. .gitignore), but skip the .git/ dir
+map("n", "<leader>ff", function()
+  require("telescope.builtin").find_files { hidden = true, file_ignore_patterns = { "%.git/" } }
+end, { desc = "telescope find files (incl. hidden)" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
